9b152bb53fa64eda28aa3d83e2af53a8c8ec4086
Zeno <144168045+Zeno79@users.noreply.github.com>
2024-09-16 00:59:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ PORT = os.environ.get("PORT", "8080")
 
 # Database
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://bajikun710:8zU61kKtCFuMmmor@zeno.yft7des.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "netflix")
+DB_NAME = os.environ.get("DATABASE_NAME", "baji")
 
 # Force sub channel id, if you want enable force sub
 FORCE_CHANNEL = int(os.environ.get("FORCE_CHANNEL", "-1001969037404"))
